876fd7eb65365cc440e491429036876f8b5e76dd
n913239 <n913239@gmail.com>
2017-08-18 21:59:37 +0800

    Lab 1.1 P333 - P342

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

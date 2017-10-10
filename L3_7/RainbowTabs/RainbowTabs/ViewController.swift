70df8fe8d3c971e831a8188574edbb7963309b45
n913239 <n913239@gmail.com>
2017-10-11 04:06:12 +0800

    add L3.7 RainbowTabs & AboutMe

---
+++ L3_7/RainbowTabs/RainbowTabs/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by mikewang on 2017/9/24.
+//  Copyright © 2017年 mike. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBarItem.badgeValue = "!"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
